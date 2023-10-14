915d67d8265f52e4fcf160b719162af1bc2b6801
Luciano Benjamín Taddeo Córdoba <87484601+iosonobenjamin@users.noreply.github.com>
2023-10-14 19:15:54 -0300

    sentencias_taddeocordoba.sql

--- sentencias_taddeocordoba.sql
+++ sentencias_taddeocordoba.sql
@@ -1,11 +1,14 @@
-CREATE USER 'user1'@'localhost' IDENTIFIED BY 'user1'; -- creacion usuario1 --
-CREATE USER 'user2'@'localhost' IDENTIFIED BY 'user2'; -- creacion usuario2 --
+-- Creación de usuario1 y usuario2 --
+CREATE USER 'user1'@'localhost' IDENTIFIED BY 'user1';
+CREATE USER 'user2'@'localhost' IDENTIFIED BY 'user2';
 
-SHOW GRANTS FOR 'user1'@'localhost'; -- muestra permisos del usuario1 --
-SHOW GRANTS FOR 'user2'@'localhost'; -- muestra permisos del usuario2 --
+-- Mostrar permisos del usuario1 y usuario2 --
+SHOW GRANTS FOR 'user1'@'localhost';
+SHOW GRANTS FOR 'user2'@'localhost';
 
-GRANT SELECT ON sistemaescolar_taddeocordoba.* TO 'user1'@'localhost'; -- se otorgan permiso de lectura las tablas de la DB --
-GRANT SELECT, INSERT, UPDATE ON sistemaescolar_taddeocordoba.* TO 'user2'@'localhost'; -- se otorgan  permiso de lectura, insertar y modificar las tablas de la DB --
--- en el comando del gran select ya se exeptua el permiso DELETE en ambos usuarios --
+-- Otorgar permisos a usuario1 y usuario2 --
+GRANT SELECT ON sistemaescolar_taddeocordoba.* TO 'user1'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON sistemaescolar_taddeocordoba.* TO 'user2'@'localhost';
 
-REVOKE DELETE ON sistemaescolar_taddeocordoba.* FROM 'user1'@'localhost','user2'@'localhost'; -- se revoca el permiso de eliminacion de registros en las tablas de la DB --
+-- Revocar permiso DELETE a usuario1 y usuario2 --
+REVOKE DELETE ON sistemaescolar_taddeocordoba.* FROM 'user1'@'localhost', 'user2'@'localhost';
