914a56ca5cc94b41ce8b39f0df947e58b96bc441
Luciano Benjamín Taddeo Córdoba <87484601+iosonobenjamin@users.noreply.github.com>
2023-10-14 19:13:54 -0300

    Stored.TaddeoCordoba.sql

--- Stored.TaddeoCordoba.sql
+++ Stored.TaddeoCordoba.sql
@@ -1,26 +1,27 @@
+DELIMITER $$
+
 -- SP 1 --
-DELIMITER $$
-
 CREATE PROCEDURE sp_APELLIDO_alumno ()
-
 BEGIN
-	SELECT
-		 *
+	SELECT *
 	FROM alumno
-	ORDER BY APELLIDO DESC LIMIT 10;
-END
-$$ 
--- SP 2 --
-DELIMITER $$
-
-CREATE PROCEDURE sp_id_legajo_alumno (id_legajo INT)  -- incrementar numero de id_legajo -- 
-
-BEGIN
-	id_legajo INT =
-	(SELECT
-		MAX (IL.id_legajo) 
-        FROM ALUMNO)+1);
-	INSERT INTO alumno (ID_LEGAJO, id_DNI, APELLIDO, NOMBRE, DIRECCION, TELEFONO, MAIL, NACIONALIDAD) 
-    VALUES (:ID_LEGAJO, :ID_DNI, :APELLIDO, :NOMBRE, :DIRECCION, :TELFONO, :MAIL, :NACIONALIDAD);
-END
+	ORDER BY APELLIDO DESC
+	LIMIT 10;
+END;
 $$
+
+-- SP 2 --
+CREATE PROCEDURE sp_id_legajo_alumno (IN id_legajo INT) -- Añadido IN para especificar que es un parámetro de entrada
+BEGIN
+	DECLARE next_id_legajo INT; -- Declaración de una variable para almacenar el próximo ID_LEGAJO
+
+	-- Obtener el próximo ID_LEGAJO
+	SELECT IFNULL(MAX(id_legajo), 0) + 1 INTO next_id_legajo FROM alumno;
+
+	-- Insertar un nuevo registro con el ID_LEGAJO proporcionado
+	INSERT INTO alumno (ID_LEGAJO, id_DNI, APELLIDO, NOMBRE, DIRECCION, TELEFONO, MAIL, NACIONALIDAD)
+	VALUES (id_legajo, :id_DNI, :APELLIDO, :NOMBRE, :DIRECCION, :TELFONO, :MAIL, :NACIONALIDAD);
+END;
+$$
+
+DELIMITER ;
