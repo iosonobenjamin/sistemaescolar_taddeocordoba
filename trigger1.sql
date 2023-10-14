364c7ccda85642fb34732a2a4998e1e2e79a2d17
Luciano Benjamín Taddeo Córdoba <87484601+iosonobenjamin@users.noreply.github.com>
2023-10-14 19:18:57 -0300

    trigger1.sql

--- trigger1.sql
+++ trigger1.sql
@@ -1,30 +1,22 @@
--- creacion tabla log --
+-- Creación de la tabla log_asistencia_cursada --
+USE sistemaescolar_taddeocordoba;
+CREATE TABLE IF NOT EXISTS log_asistencia_cursada (
+  ID_LOG INT NOT NULL AUTO_INCREMENT,
+  ACCION VARCHAR(45) NOT NULL,
+  USUARIO VARCHAR(45) NOT NULL,
+  DETALLE VARCHAR(100) NOT NULL,
+  FECHA DATETIME NOT NULL,
+  PRIMARY KEY (ID_LOG)
+);
 
-USE sistemaescolar_taddeocordoba
-
--- creacion de la tabla log --
-
-CREATE TABLE `sistemaescolar_taddeocordoba`.`log_asistencia_cursada` (
-  `ID_LOG` INT NOT NULL AUTO_INCREMENT,
-  `ACCION` VARCHAR(45) NOT NULL,
-  `USUARIO` VARCHAR(45) NOT NULL,
-  `DETALLE` VARCHAR(45) NOT NULL,
-  `FECHA` DATETIME NOT NULL,
-  PRIMARY KEY (`ID_LOG`));
-
--- creacion del log de auditoria de la tabala asistencia_cursada --
-
+-- Creación del trigger para el log de auditoría de la tabla asistencia_cursada --
 DELIMITER $$
-CREATE TRIGGER LOG_ASISTENCIA_CURSADA
+CREATE TRIGGER log_after_insert_asistencia_cursada
 AFTER INSERT ON asistencia_cursada
 FOR EACH ROW
 BEGIN
-	INSERT INTO LOG_ASISTENCIA_CURSADA (ID_LOG, ACCION, USUARIO, DETALLE, FECHA)
-    VALUES
-    (NULL, 'INSERT', SYSTEM_USER(), CONCAT ('SE INGRESO LA ASISTENCIA', ID_LOG, 'PARA EL ALUMNO'), NOW());
-END $$
+	INSERT INTO log_asistencia_cursada (ID_LOG, ACCION, USUARIO, DETALLE, FECHA)
+	VALUES (NULL, 'INSERT', CURRENT_USER(), CONCAT('SE INGRESÓ LA ASISTENCIA PARA EL ALUMNO CON ID_LOG = ', NEW.ID_LOG), NOW());
+END
+$$
 DELIMITER ;
-
-
-
-
